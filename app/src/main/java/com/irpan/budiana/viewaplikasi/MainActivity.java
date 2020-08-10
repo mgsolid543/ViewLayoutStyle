66ca7b8e8e05c3bb7cce58438434931bce8e98a3
Irpan Budiana <irpan.budiana@gmail.com>
2020-08-10 23:28:58 +0700

    first commit

---
+++ app/src/main/java/com/irpan/budiana/viewaplikasi/MainActivity.java
@@ -0,0 +1,17 @@
+package com.irpan.budiana.viewaplikasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Huawei P30 Pro");
+        }
+    }
+}
